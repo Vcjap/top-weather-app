da2bf3eff2744f7fe64826d60dbba3276d5e7e88
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-02-23 12:08:16 +0100

    Refactor button event to use one function, and keep system consistent

--- src/index.js
+++ src/index.js
@@ -2,41 +2,30 @@ import "./styles.css";
 import weather from "./weather.js";
 import dom from "./dom.js";
 
-
+let forecastSystem = "metric";
 
 const searchLocationBtn = document.getElementById("search_location");
-searchLocationBtn.addEventListener("click", async (event) => {
+searchLocationBtn.addEventListener("click", async (event) => showForecast(event, forecastSystem));
+
+const celsiusBtn = document.getElementById("metric");
+celsiusBtn.addEventListener("click", async (event) => showForecast(event, event.target.id));
+
+const fahrenheitBtn = document.getElementById("us")
+fahrenheitBtn.addEventListener("click", async (event) => showForecast(event, event.target.id));
+
+const showForecast = async (event, newForecastSystem) => {
     event.preventDefault();
-    dom.refreshDisplay([{}]);
-    const queryInput = document.getElementById("query");
-    const location = queryInput.value;
+    dom.refreshDisplay([{}]); // Show the loader while we fetch data
 
     try {
-        const forecast = await weather.getForecast(location, 3, "metric");
+        const queryInput = document.getElementById("query");
+        const location = queryInput.value;
+        forecastSystem = newForecastSystem; // Used to keep or change celsius/fahrenheit, depending on the button clicked
+        const forecast = await weather.getForecast(location, 3, forecastSystem);
         dom.refreshDisplay(forecast);
     } catch {
         const container = document.getElementById("forecast_container");
         container.innerHTML = "";
         container.textContent = "Location not found. Try another one!";
     }
-})
-
-const celsiusBtn = document.getElementById("metric");
-celsiusBtn.addEventListener("click", async (event) => {
-    event.preventDefault();
-    dom.refreshDisplay([{}]);
-    const queryInput = document.getElementById("query");
-    const location = queryInput.value;
-    const forecast = await weather.getForecast(location, 3, event.target.id);
-    dom.refreshDisplay(forecast);
-});
-
-const fahrenheitBtn = document.getElementById("us")
-fahrenheitBtn.addEventListener("click", async (event) => {
-    event.preventDefault();
-    dom.refreshDisplay([{}]);
-    const queryInput = document.getElementById("query");
-    const location = queryInput.value;
-    const forecast = await weather.getForecast(location, 3, event.target.id);
-    dom.refreshDisplay(forecast);
-});
+}
